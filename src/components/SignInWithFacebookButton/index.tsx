22256102764eef8a0b4810c76b4b51f42d0dd41e
auadmendes <luciano.auad@gmail.com>
2022-12-15 12:01:02 -0300

    facebook button alert

--- src/components/SignInWithFacebookButton/index.tsx
+++ src/components/SignInWithFacebookButton/index.tsx
@@ -1,7 +1,15 @@
 import { FaFacebook } from 'react-icons/fa'
 export function SignInWithFacebookButton() {
+
+  function buttonNotWorking() {
+    alert('Sorry! This method was not implemented')
+  }
+
   return (
-    <button type="button" className='ml-auto text-blue-400'>
+    <button
+      onClick={buttonNotWorking}
+      type="button"
+      className='ml-auto text-blue-400'>
       <FaFacebook size={32} />
     </button>
   )
